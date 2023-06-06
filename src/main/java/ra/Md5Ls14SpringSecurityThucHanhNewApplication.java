91787162a2d17dfac0775f1ba06b7aeffd4cde0a
ThangNP03 <2003nguyenphucthang@gmail.com>
2023-06-06 18:09:01 +0700

    hhhsg

---
+++ src/main/java/ra/Md5Ls14SpringSecurityThucHanhNewApplication.java
@@ -0,0 +1,13 @@
+package ra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Md5Ls14SpringSecurityThucHanhNewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Md5Ls14SpringSecurityThucHanhNewApplication.class, args);
+    }
+
+}
---
+++ src/main/java/ra/model/service/IRoleService.java
@@ -0,0 +1,10 @@
+package ra.model.service;
+
+import ra.model.entity.ERole;
+import ra.model.entity.Roles;
+
+import java.util.Optional;
+
+public interface IRoleService {
+    Optional<Roles> findByRoleName(ERole roleName);
+}
